8a38cbd842474589685cab529cf581d4c43c81c7
Willem van der Schyff <willemvds@gmail.com>
2025-12-27 14:30:23 +0200

    feat: Add move-to-workspace 1-4.

--- cli/gnome.go
+++ cli/gnome.go
@@ -16,28 +16,28 @@ func GnomeSettings(logger *slog.Logger, stdout io.Writer, stderr io.Writer) erro
 	fmt.Fprintf(stdout, "[ * ] Updating GNOME Settings (keybinds).\n")
 	ansiseq.Reset(stdout)
 
+	keybinds := map[string]string{
+		"switch-to-workspace-1": "<Super>1",
+		"switch-to-workspace-2": "<Super>2",
+		"switch-to-workspace-3": "<Super>3",
+		"switch-to-workspace-4": "<Super>4",
+		"move-to-workspace-1":   "<Super><Shift>1",
+		"move-to-workspace-2":   "<Super><Shift>2",
+		"move-to-workspace-3":   "<Super><Shift>3",
+		"move-to-workspace-4":   "<Super><Shift>4",
+	}
+
 	ctx := context.Background()
-	err1 := jobs.GnomeKeybind(ctx, logger, "switch-to-workspace-1", "<Super>1")
-	if err1 != nil {
-		fmt.Fprintf(stdout, "[ F ] (switch-to-workspace-1) %s.\n", err1)
+	hasFailures := false
+	for action, bind := range keybinds {
+		err := jobs.GnomeKeybind(ctx, logger, action, bind)
+		if err != nil {
+			hasFailures = true
+			fmt.Fprintf(stdout, "[ F ] (%s->%s) %s.\n", action, bind, err)
+		}
 	}
 
-	err2 := jobs.GnomeKeybind(ctx, logger, "switch-to-workspace-2", "<Super>2")
-	if err2 != nil {
-		fmt.Fprintf(stdout, "[ F ] (switch-to-workspace-2) %s.\n", err2)
-	}
-
-	err3 := jobs.GnomeKeybind(ctx, logger, "switch-to-workspace-3", "<Super>3")
-	if err3 != nil {
-		fmt.Fprintf(stdout, "[ F ] (switch-to-workspace-3) %s.\n", err3)
-	}
-
-	err4 := jobs.GnomeKeybind(ctx, logger, "switch-to-workspace-4", "<Super>4")
-	if err4 != nil {
-		fmt.Fprintf(stdout, "[ F ] (switch-to-workspace-4) %s.\n", err4)
-	}
-
-	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+	if hasFailures {
 		return errors.New("1 or more keybinds failed")
 	}
 
